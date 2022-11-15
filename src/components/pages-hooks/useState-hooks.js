989d35a08c060296655fae37a166a90b108a9fc6
Maria Mys <92423190+MaryMisMaria@users.noreply.github.com>
2022-11-15 16:03:37 +0200

    Update useState-hooks.js

--- src/components/pages-hooks/useState-hooks.js
+++ src/components/pages-hooks/useState-hooks.js
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 // material
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import List from '@mui/material/List'
+import { Button } from '@mui/material'
 import Divider from '@mui/material/Divider'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
@@ -9,8 +11,21 @@ import CardContent from '@mui/material/CardContent'
 import ListItemText from '@mui/material/ListItemText'
 // images
 import BackgroundImage from '../../images/1.png'
+// icons material
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ThumbDownIcon from '@mui/icons-material/ThumbDown'
 
 const HooksUseState = () => {
+  const [count, setCount] = useState(0)
+  const [iconTwoColor, setIconTwoColor] = useState('green')
+
+  const PurpleColor = () => {
+    setIconTwoColor('purple')
+  }
+
+  const BlackColor = () => {
+    setIconTwoColor('black')
+  }
 
   return (
     <Box align='center'>
@@ -36,26 +51,62 @@ const HooksUseState = () => {
           </ListItemText>
         </List>
       </Box>
-      <Card pb={4} sx={{ maxWidth: '80%', maxHeight: '50%' }}>
-        <CardMedia
-          height='200px'
-          component='img'
-          sx={{ backgroundImage: `url(${BackgroundImage})` }}
-        />
-        <CardContent>
-          <Typography variant='body2' color='text.secondary'>
-						const [state, setState] = useState(initialState);
-						Повертає значення стану та функцію, що оновлює його.
-						Під час початкового рендеру повернутий стан (state) співпадає зі значенням,
-						переданим у першому аргументі (initialState).
-						Функція setState використовується для оновлення стану.
-						Вона приймає значення нового стану і ставить у чергу повторний рендер компонента.
-						setState(newState);
-						Упродовж наступних повторних рендерів, перше значення, повернуте useState,
-						завжди буде в актуальному стані при здійсненні оновлень.
-          </Typography>
-        </CardContent>
-      </Card>
+      <Box width='80%' display='flex' bgcolor='background.paper'>
+        <Card pb={4} sx={{ width: '50%', height: '50%' }}>
+          <CardMedia
+            height='200px'
+            component='img'
+            sx={{ backgroundImage: `url(${BackgroundImage})` }}
+          />
+          <CardContent>
+            <Typography variant='body2' color='text.secondary'>
+							const [state, setState] = useState(initialState);
+							Повертає значення стану та функцію, що оновлює його.
+							Під час початкового рендеру повернутий стан (state) співпадає зі значенням,
+							переданим у першому аргументі (initialState).
+							Функція setState використовується для оновлення стану.
+							Вона приймає значення нового стану і ставить у чергу повторний рендер компонента.
+							setState(newState);
+							Упродовж наступних повторних рендерів, перше значення, повернуте useState,
+							завжди буде в актуальному стані при здійсненні оновлень.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Box width='50%' textAlign='center'>
+          <Typography>Ви натиснули {count} разів</Typography>
+          <Box mt={5} display='flex' justifyContent='space-evenly'>
+            <Button
+              variant='outlined'
+              onClick={() => setCount(count - 1)}
+              sx={{ bgcolor: '#081627', color: 'white', borderRadius: '3px' }}
+            >
+							Increase value
+            </Button>
+            <Button
+              variant='outlined'
+              onClick={() => setCount(count + 1)}
+              sx={{ bgcolor: '#081627', height: '50%', color: 'white', borderRadius: '3px' }}
+            >
+							Reduce value
+            </Button>
+          </Box>
+          <Typography mt={3}>При кліку змінюється колір</Typography>
+          <Box mt={5} display='flex' justifyContent='space-evenly'>
+            <button
+              style={{ color: iconTwoColor }}
+              onClick={PurpleColor}
+            >
+              <FavoriteIcon />
+            </button>
+            <button
+              style={{ color: iconTwoColor }}
+              onClick={BlackColor}
+            >
+              <ThumbDownIcon />
+            </button>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   )
 }
