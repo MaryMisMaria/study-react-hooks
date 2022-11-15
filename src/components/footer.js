92a957fca271e833c1949c609d1e555c4d2c6a61
Maria Mys <92423190+MaryMisMaria@users.noreply.github.com>
2022-11-15 09:06:38 +0200

    main

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,21 @@
-import React  from 'react'
 import { Box, Link, Typography } from '@mui/material'
 
 const Footer = () => {
-    return (
-      <Box sx={{
-        width: '100%',
-        height: '100px',
-        display: 'flex',
-        alignItems: 'center',
-        background: '#081627',
-        justifyContent: 'center',
-      }}
-      >
-        <Link href='https://uk.reactjs.org/docs/hooks-effect.html'>
-          <Typography color='white' variant='h6'>Links on documentation</Typography>
-        </Link>
-      </Box>
-    )
+  return (
+    <Box sx={{
+      width: '100%',
+      height: '100px',
+      display: 'flex',
+      alignItems: 'center',
+      background: '#081627',
+      justifyContent: 'center',
+    }}
+    >
+      <Link href='https://uk.reactjs.org/docs/hooks-effect.html'>
+        <Typography color='white' variant='h6'>Links on documentation</Typography>
+      </Link>
+    </Box>
+  )
 }
 
 export default Footer
